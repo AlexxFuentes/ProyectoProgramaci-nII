815add25ae2e342c531f54e405d037956e25bab3
value <65086190+AlexxFuentes@users.noreply.github.com>
2020-07-16 16:41:09 -0600

    Carga gestionUSuario

--- TransaccionesBancarias/src/GestionClases/GestionUsuario.java
+++ TransaccionesBancarias/src/GestionClases/GestionUsuario.java
@@ -1,5 +1,42 @@
 package GestionClases;
 
+import java.util.LinkedList;
+
+import clases.Usuario;
+import clases.NumeroCuenta;
+
+/**
+ * CLASE GESTIÓN DE USUARIO: DONDE SE ADMINISTRAN LOS DATOS DEL USUARIO
+ * @author alex Fuentes
+ * 
+ *FECHA: 16/07/2020
+ */
+
 public class GestionUsuario {
+	
+	//ATRIBUTOS
+	private LinkedList<Usuario> listaUsuario = new LinkedList<>();
+	
+	public LinkedList<Usuario> getEstudiante(){
+		return listaUsuario;
+	}
+	
+	/**
+	 * MÉTODO QUE BUSCA USUARIO POR NÚMERO DE CUENTA
+	 * @param numCuenta
+	 * @return null - SI NO ENCUENTRA AL USUARIO, CASO CONTRARIO RETORNA OBJETOUSUARIO 
+	 */
+	public Usuario BuscarUsuario(NumeroCuenta numCuenta) {
+		Usuario usuarioBuscado = null;
+		
+		for(int i = 0; i < listaUsuario.size(); i++) {
+			if( listaUsuario.get(i).get_numerocuenta().equals(numCuenta) ) {
+				usuarioBuscado = listaUsuario.get(i);
+				
+			}
+		}
+		return usuarioBuscado;
+	}
+	
 
 }
--- TransaccionesBancarias/src/clases/Usuario.java
+++ TransaccionesBancarias/src/clases/Usuario.java
@@ -13,109 +13,124 @@ public class Usuario {
 	private String _apellido;
 	private String _direccion;
 	private String _telefono;
+	private NumeroCuenta _numerocuenta;
 	private String _contrasena;
 	private String _rtn;
+	private String _correoelectronico;
 	private TipoCuenta _tipoCuenta;
+	private CrearCuenta _crearcuenta;
 	
-	
+	//METODO CONSTRUCTOR POR DEFECTO
 	public Usuario() {
-		
-	}
-
-
-	public Usuario(String nombre, String apellido, String direccion, String telefono, String contrasena,
-			String rtn, TipoCuenta tipoCuenta) {
 		super();
-		this._nombre = nombre;
-		this._apellido = apellido;
-		this._direccion = direccion;
-		this._telefono = telefono;
-		this._contrasena = contrasena;
-		this._rtn = rtn;
-		this._tipoCuenta = tipoCuenta;
 	}
 
+	//METODO CONSTRUCTO
+	public Usuario(String _nombre, String _apellido, String _direccion, String _telefono, NumeroCuenta _numerocuenta,
+			String _contrasena, String _rtn, String _correoelectronico, TipoCuenta _tipoCuenta,
+			CrearCuenta _crearcuenta) {
+		super();
+		this._nombre = _nombre;
+		this._apellido = _apellido;
+		this._direccion = _direccion;
+		this._telefono = _telefono;
+		this._numerocuenta = _numerocuenta;
+		this._contrasena = _contrasena;
+		this._rtn = _rtn;
+		this._correoelectronico = _correoelectronico;
+		this._tipoCuenta = _tipoCuenta;
+		this._crearcuenta = _crearcuenta;
+	}
 
+	//GETTER AND SETTER
+	
 	public String get_nombre() {
 		return _nombre;
 	}
-
 
 	public void set_nombre(String _nombre) {
 		this._nombre = _nombre;
 	}
 
-
 	public String get_apellido() {
 		return _apellido;
 	}
-
 
 	public void set_apellido(String _apellido) {
 		this._apellido = _apellido;
 	}
 
-
 	public String get_direccion() {
 		return _direccion;
 	}
-
 
 	public void set_direccion(String _direccion) {
 		this._direccion = _direccion;
 	}
 
-
 	public String get_telefono() {
 		return _telefono;
 	}
-
 
 	public void set_telefono(String _telefono) {
 		this._telefono = _telefono;
 	}
 
+	public NumeroCuenta get_numerocuenta() {
+		return _numerocuenta;
+	}
+
+	public void set_numerocuenta(NumeroCuenta _numerocuenta) {
+		this._numerocuenta = _numerocuenta;
+	}
 
 	public String get_contrasena() {
 		return _contrasena;
 	}
 
-
 	public void set_contrasena(String _contrasena) {
 		this._contrasena = _contrasena;
 	}
-
 
 	public String get_rtn() {
 		return _rtn;
 	}
 
-
 	public void set_rtn(String _rtn) {
 		this._rtn = _rtn;
 	}
 
+	public String get_correoelectronico() {
+		return _correoelectronico;
+	}
+
+	public void set_correoelectronico(String _correoelectronico) {
+		this._correoelectronico = _correoelectronico;
+	}
 
 	public TipoCuenta get_tipoCuenta() {
 		return _tipoCuenta;
 	}
 
-
 	public void set_tipoCuenta(TipoCuenta _tipoCuenta) {
 		this._tipoCuenta = _tipoCuenta;
 	}
-	
-	
 
+	public CrearCuenta get_crearcuenta() {
+		return _crearcuenta;
+	}
 
+	public void set_crearcuenta(CrearCuenta _crearcuenta) {
+		this._crearcuenta = _crearcuenta;
+	}
+
+	//SOBREESCRITURA DEL TOSTRING
+	@Override
 	public String toString() {
 		return "Usuario [_nombre=" + _nombre + ", _apellido=" + _apellido + ", _direccion=" + _direccion
-				+ ", _telefono=" + _telefono + ", _contrasena=" + _contrasena + ", _rtn=" + _rtn + ", _tipoCuenta="
-				+ _tipoCuenta + "]";
+				+ ", _telefono=" + _telefono + ", _numerocuenta=" + _numerocuenta + ", _contrasena=" + _contrasena
+				+ ", _rtn=" + _rtn + ", _correoelectronico=" + _correoelectronico + ", _tipoCuenta=" + _tipoCuenta
+				+ ", _crearcuenta=" + _crearcuenta + "]";
 	}
 	
-	
-	
-	
-
 }
