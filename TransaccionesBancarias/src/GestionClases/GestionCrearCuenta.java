86b0503b10626b8bcdd4cb764c455bbd8b4f7687
value <65086190+AlexxFuentes@users.noreply.github.com>
2020-07-16 22:36:56 -0600

    GESTIÓN CREAR CUENTA

---
+++ TransaccionesBancarias/src/GestionClases/GestionCrearCuenta.java
@@ -0,0 +1,105 @@
+package GestionClases;
+
+import java.util.LinkedList;
+
+import clases.CrearCuenta;
+import clases.NumeroCuenta;
+
+/**
+ * CLASE QUE CREA UNA NUEVA CUENTA PARA EL USUARIO
+ * @author Alex Fuentes
+ *FECHA: 16/07/2020
+ */
+
+public class GestionCrearCuenta {
+	
+	//ATRIBUTO (COLECCIÓN)
+	LinkedList<CrearCuenta> listaCrearCuenta = new LinkedList<>();
+	
+	
+	/**
+	 * MÉTODO QUE OBTIENE CREAR CUENTA DE LA COLECCIÓN
+	 * @return listaCrearCuenta
+	 */
+	public LinkedList<CrearCuenta> getCrearCuenta(){
+		return listaCrearCuenta;
+	}
+
+	/**
+	 * MÉTODO QUE BUSCA CUENTA EXISTENTE
+	 * @param numCuenta
+	 * @return null - SI NO EXISTE LA CUENTA, CASO CONTRARIO return OBJETOCREARCUENTA
+	 */
+	public CrearCuenta BuscarCuenta(NumeroCuenta numCuenta) {//busca Usuario por número de cuenta
+		CrearCuenta cuentaBuscar = null;
+		
+		for(int i = 0; i < listaCrearCuenta.size(); i++) {
+			if( listaCrearCuenta.get(i).get_usuario().get_numerocuenta().equals(numCuenta) ) {
+				cuentaBuscar = listaCrearCuenta.get(i);
+			}
+		}
+		return cuentaBuscar;
+	}
+	/**
+	 * MÉTODO QUE CREA NUEVA CUENTA
+	 * @param crearcuenta
+	 * @return true - SI SE CREA LA CUENTA, CASO CONTRARIO return false
+	 */
+	public boolean AgregarUsuario(CrearCuenta crearcuenta) {
+		
+		if(BuscarCuenta(crearcuenta.get_usuario().get_numerocuenta()) == null) {//Si el usuario no esta agregado
+			listaCrearCuenta.add(crearcuenta);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	/**
+	 * MÉTODO QUE ELIMINA USUARIO CREADO 
+	 * @param numCuenta
+	 * @return true - SI SE ELIMINO CORRECTAMENTE, CASO CONTRARIO return false.
+	 */
+	public boolean EliminarUsuarioCreado(NumeroCuenta numCuenta) {
+		CrearCuenta UsuarioEncontrado = null;
+		UsuarioEncontrado = BuscarCuenta(numCuenta);
+		
+		if(UsuarioEncontrado == null) {
+			return false;
+		}else {
+			listaCrearCuenta.remove(BuscarCuenta(numCuenta));
+			return true;
+		}
+	}
+	 
+	/**
+	 * MÉTODO QUE MODIFICA CUENTA CREADA
+	 * @param usuarioModificado
+	 * @param posicion
+	 * @return true - SI SE MODIFICO CORRECTAMENTE, CASO CONTRARIO return false
+	 */
+	public boolean ModificarUsuarioCreado(CrearCuenta usuarioModificado, int posicion) {
+		
+		if(posicion<0 || posicion>listaCrearCuenta.size()) {
+			return false;
+		}else {
+			CrearCuenta UsuarioEncontrado = BuscarCuenta(usuarioModificado.get_usuario().get_numerocuenta());
+	
+			if(UsuarioEncontrado != null) {
+				listaCrearCuenta.set(posicion, usuarioModificado);
+				return true;
+			}else {
+				return false;
+			}
+		
+		}
+	} 
+	/**
+	 * MÉTODO PARA OBTENER POSICIÓN DE LA COLECCIÓN CrearCuenta
+	 * @param posicion
+	 * @return POSICIÓN O INDICE DE LA COLECIÓN
+	 */
+	public CrearCuenta getPosicion(int posicion) {
+		return listaCrearCuenta.get(posicion);
+	}
+}
--- TransaccionesBancarias/src/clases/CrearCuenta.java
+++ TransaccionesBancarias/src/clases/CrearCuenta.java
@@ -11,48 +11,35 @@ package clases;
  * */
 
 public class CrearCuenta {
+	
+	//ATRIBUTOS
 	private Usuario _usuario;
-	private NumeroCuenta _numerocuenta;
-	private TipoCuenta _tipocuenta;
 	private double _montoinicial;
+	private TipoCuenta _tipocuenta;
 	private Referencia _referencia;
 	
+	//CONSTRUCTOR POR DEFECTO
 	public CrearCuenta() {
-		
-	}
-
-	public CrearCuenta(Usuario usuario, NumeroCuenta numerocuenta, TipoCuenta tipocuenta, double montoinicial,
-			Referencia referencia) {
 		super();
-		this._usuario = usuario;
-		this._numerocuenta = numerocuenta;
-		this._tipocuenta = tipocuenta;
-		this._montoinicial = montoinicial;
-		this._referencia = referencia;
 	}
 
+	//CONSTRUCTOR DE LA CLASE
+	public CrearCuenta(Usuario _usuario, double _montoinicial, TipoCuenta _tipocuenta, Referencia _referencia) {
+		super();
+		this._usuario = _usuario;
+		this._montoinicial = _montoinicial;
+		this._tipocuenta = _tipocuenta;
+		this._referencia = _referencia;
+	}
+
+	
+	//GETTER AND SETTER
 	public Usuario get_usuario() {
 		return _usuario;
 	}
 
 	public void set_usuario(Usuario _usuario) {
 		this._usuario = _usuario;
-	}
-
-	public NumeroCuenta get_numerocuenta() {
-		return _numerocuenta;
-	}
-
-	public void set_numerocuenta(NumeroCuenta _numerocuenta) {
-		this._numerocuenta = _numerocuenta;
-	}
-
-	public TipoCuenta get_tipocuenta() {
-		return _tipocuenta;
-	}
-
-	public void set_tipocuenta(TipoCuenta _tipocuenta) {
-		this._tipocuenta = _tipocuenta;
 	}
 
 	public double get_montoinicial() {
@@ -63,6 +50,14 @@ public class CrearCuenta {
 		this._montoinicial = _montoinicial;
 	}
 
+	public TipoCuenta get_tipocuenta() {
+		return _tipocuenta;
+	}
+
+	public void set_tipocuenta(TipoCuenta _tipocuenta) {
+		this._tipocuenta = _tipocuenta;
+	}
+
 	public Referencia get_referencia() {
 		return _referencia;
 	}
@@ -71,10 +66,12 @@ public class CrearCuenta {
 		this._referencia = _referencia;
 	}
 
-	
+	//S0BREESCRITURA DE TOSTRING
+	@Override
 	public String toString() {
-		return "CrearCuenta [_usuario=" + _usuario + ", _numerocuenta=" + _numerocuenta + ", _tipocuenta=" + _tipocuenta
-				+ ", _montoinicial=" + _montoinicial + ", _referencia=" + _referencia + "]";
+		return "CrearCuenta [_usuario=" + _usuario + ", _montoinicial=" + _montoinicial + ", _tipocuenta=" + _tipocuenta
+				+ ", _referencia=" + _referencia + "]";
 	}
-
+	
+	
 }
