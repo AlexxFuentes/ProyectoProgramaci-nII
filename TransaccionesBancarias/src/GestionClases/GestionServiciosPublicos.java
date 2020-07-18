0aa856f92160562768738654ab74b1f562480d64
GlenyNihimaya <65729945+GlenyNihimaya@users.noreply.github.com>
2020-07-18 10:00:03 -0600

    new gest

--- TransaccionesBancarias/src/GestionClases/GestionServiciosPublicos.java
+++ TransaccionesBancarias/src/GestionClases/GestionServiciosPublicos.java
@@ -2,6 +2,39 @@ package GestionClases;
 
 import java.util.LinkedList;
 
+import clases.Retirar;
+import clases.ServiciosPublicos;
+
 public class GestionServiciosPublicos {
+	private LinkedList<ServiciosPublicos> listaServiciosPublico = new LinkedList<>();
+
+	public GestionServiciosPublicos() {
+		super();
+	}
+
+	public LinkedList<ServiciosPublicos> getListaServiciosPublico() {
+		return listaServiciosPublico;
+	}
+
+	public void setListaServiciosPublico(LinkedList<ServiciosPublicos> listaServiciosPublico) {
+		this.listaServiciosPublico = listaServiciosPublico;
+	}
+	
+	/**
+	 * METODO UTILIZADO PARA BUSCAR SERVICIO PUBLICO
+	 * @param retModificado
+	 * @return usuarioEncontrado
+	 */
+	public ServiciosPublicos BuscarServiciosPublicos(ServiciosPublicos servicio) {
+		ServiciosPublicos MontoBuscado = null;
+		
+		for(int i=0;i<listaServiciosPublico.size();i++) {
+			if(listaServiciosPublico.get(i).get_aguaPotable() == monto) {
+				MontoBuscado = listaServiciosPublico.get(i);
+				break;
+			}
+		}
+		return MontoBuscado;
+	}
 	
 }
